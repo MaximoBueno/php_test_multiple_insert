06aa2a91ef5f8ba36b549488cd663160c2b5c6f4
Maximo Bueno <maxhugobueno@gmail.com>
2022-03-15 20:06:37 -0500

    Error Multiple Insert
    Agrego función limpiar memoria

--- globalconexion.php
+++ globalconexion.php
@@ -1,20 +1,46 @@
 <?php
+//Nota: Estos errores solo se ven al no aplicar ningun ORM
+//desplegar todos los errores de mysql al realizar pruebas
+mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 
-$gcon = new mysqli("localhost",
-        "root", 
-        "",
-        "ps_crud_max",
-        3306);
+$gcon = new mysqli(
+    "localhost",
+    "root",
+    "",
+    "ps_crud_max",
+    3306
+);
 
 global $gcon;
 
-function ejecutarConsulta($sql){
+function ejecutarConsulta($sql)
+{
     global $gcon;
+
+    limpiarMemoria();
+
     return $gcon->query($sql);
 }
 
-echo "Global Conexion Test:<br>";
-var_dump(ejecutarConsulta("INSERT INTO persona(nombre,telefono) VALUES('global_con_1','aaa'),('global_con_1.5','aaa');"));
-var_dump(ejecutarConsulta("INSERT INTO persona(nombre,telefono) VALUES('global_con_1','bb');"));
+//Limpiar la memoria cuando un objeto o resultado se queda en la instancia del mysqli para evitar bloqueos de consulta.
+function limpiarMemoria()
+{
+    global $gcon;
 
-?>
+    while ($gcon->more_results()) {
+        $gcon->next_result();
+        if ($res = $gcon->store_result()) {
+            $res->free();
+        }
+    }
+}
+
+
+
+echo "Global Conexion Test:<br>";
+
+$miInsert = "INSERT INTO persona(nombre,telefono) VALUES('global_con_1','aaa'), ('global_con_1.5','bb');";
+$miSP = "CALL sp_persona ('aa', 'bb');";
+
+var_dump(ejecutarConsulta($miSP));
+var_dump(ejecutarConsulta($miInsert));
